3f9eb121bbeccbe9e7dfb920dc7406b9694f66c8
Simone Veitch <simone.veitch@gmail.com>
2024-05-21 10:56:13 +1000

    Completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  const [selectedPizza, setSelectedPizza] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+      .then((r) => r.json())
+      .then((data) => {
+        setPizzas(data);
+      });
+  }, []);
+ 
+  const onEditPizza = (pizza) => {
+    setSelectedPizza(pizza);
+  };
+
+  const handleSubmit = (editedPizza) => {
+    // Update state with edited pizza
+    const updatedPizzas = pizzas.map((pizza) =>
+      pizza.id === editedPizza.id ? editedPizza : pizza
+    );
+    setPizzas(updatedPizzas);
+
+    // Make a request to update the backend
+  fetch(`http://localhost:3001/pizzas/${editedPizza.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editedPizza),
+  });
+};
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm pizza={selectedPizza} onSubmit={handleSubmit} />
+      <PizzaList pizzas={pizzas} onEditPizza={onEditPizza} />
     </>
   );
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function PizzaForm() {
+function PizzaForm({pizza, onSubmit}) {
+  const [formData, setFormData] = useState({ topping: "", size: "", vegetarian: "" });
+
+  useEffect(() => {
+    if (pizza) {
+      setFormData(pizza);
+    }
+  }, [pizza]);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="col-5">
           <input
             className="form-control"
             type="text"
             name="topping"
+            value={formData.topping}
+            onChange={handleChange}
             placeholder="Pizza Topping"
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select 
+          className="form-control"
+          name="size"
+          value={formData.size}
+          onChange={handleChange}
+          >
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -26,6 +50,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              checked={formData.vegetarian === "Vegetarian"}
+              onChange={handleChange}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -35,6 +61,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={formData.vegetarian === "Not Vegetarian"}
+              onChange={handleChange}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
